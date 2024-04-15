84270d222450ca0264e7dacedf628c03fef7a2db
ductuan <76565270+jetman18@users.noreply.github.com>
2024-04-16 00:56:55 +0700

    commit

--- fgcontrl/controller.py
+++ fgcontrl/controller.py
@@ -1,16 +1,16 @@
 def control(roll,pitch,yaw,latitude,longitude,altitude):
     str_ =''
-    str_ += str(roll)
-    str_ +='\t'
-    str_ += str(pitch)
-    str_ +='\t'
-    str_ += str(yaw)
-    str_ +='\t'
     str_ += str(latitude)
     str_ +='\t'
     str_ += str(longitude)
     str_ +='\t'
     str_ += str(altitude*3.280839895)
+    str_ +='\t'
+    str_ += str(roll)
+    str_ +='\t'
+    str_ += str(pitch)
+    str_ +='\t'
+    str_ += str(yaw)
     str_ +='\n'
     bytess = bytes(str_, 'utf-8')
     return bytess
--- fgcontrl/main.py
+++ fgcontrl/main.py
@@ -2,13 +2,23 @@ from pymavlink import mavutil
 import time
 import socket
 from controller import *
-master = mavutil.mavlink_connection("COM13",baud=115200)
+try:
+    master = mavutil.mavlink_connection("COM13",baud=115200)
+except:
+    pass
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('127.0.0.1', 12000))
 addr = ("127.0.0.1",5500)
 
+init_latitude  = 37.472
+init_longitude = -121.170
+
+t = 0
+
 while True:
     try:
+
+         
         data = master.recv_match().to_dict()
         roll = data['param1'] 
         pitch = data['param2'] 
@@ -16,7 +26,14 @@ while True:
         lat = data['param4']
         lon = data['param5']
         alt = data['param6']
+        
+        #print(int(roll),' ',int(pitch),' ',int(alt))
+
+        #buffer = control(init_longitude,init_latitude,0,0,0,0)
         buffer = control(roll,pitch,yaw,lat,lon,alt)
         server_socket.sendto(buffer,addr)
     except:
         pass
+    while(time.time() - t < 0.001):
+        pass
+    t = time.time()
